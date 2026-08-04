698ea5e59ef11f7cd7490c352d39ed3f787bcd64
26tscole <123657398+26tscole@users.noreply.github.com>
2026-08-04 01:20:08 -0400

    probably not gonna use the camera script i have to find a new one to use

--- Assets/Scripts/Enitites/Player/PlayerCamera.cs
+++ Assets/Scripts/Enitites/Player/PlayerCamera.cs
@@ -3,19 +3,18 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
 
-    [SerializeField] private Transform playerTransform;
-    private Collider col;
+    public Transform playerTransform;
+    private Vector3 lastPlayerPosition;
 
     void Start()
     {
-        col = playerTransform.GetComponent<Collider>();
-        float playerHeight = col.bounds.size.y;
-        transform.position = new Vector3(playerTransform.position.x, playerHeight + 5f, playerTransform.position.z - 30f);
+        lastPlayerPosition = playerTransform.position;
+        transform.position = new Vector3(lastPlayerPosition.x, lastPlayerPosition.y + 20f, lastPlayerPosition.z - 30f);
     }
 
     void LateUpdate()
     {
-        float playerHeight = col.bounds.size.y;
-        transform.position = new Vector3(playerTransform.position.x, playerHeight + 5f, playerTransform.position.z - 30f);
+        transform.position = new Vector3(playerTransform.position.x, lastPlayerPosition.y + 20f, playerTransform.position.z - 30f);
+        lastPlayerPosition = playerTransform.position;
     }
 }
