c20e24b3b485c1ca8f603e56f1ed4a02572229f3
26tscole <123657398+26tscole@users.noreply.github.com>
2026-08-07 00:00:46 -0400

    I have to stop working on the movement but I think this is fine for now

--- Assets/Scripts/Enitites/Player/Movement.cs
+++ Assets/Scripts/Enitites/Player/Movement.cs
@@ -3,40 +3,48 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    public Transform playerShip;
+    public float forwardSpeed = 25f, strafeSpeed = 10f, hoverSpeed = 5f;
+    public float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
+    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
 
-    public Rigidbody rb;
+    public float lookSpeed = 90f;
+    private Vector2 lookInput, screenCenter, mouseDistance;
 
-    public float turnSpeed = 60f;
-    public float boostSpeed = 45f;
+    public float rollSpeed = 90f, rollAcceleration = 3.5f;
+    private float rollInput;
+
 
     public void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.useGravity = true;
+        screenCenter.x = Screen.width * 0.5f;
+        screenCenter.y = Screen.height * 0.5f;
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false; 
     }
 
 
-    void FixedUpdate()
+    void Update()
     {
-        turn();
-        thrust();
+        lookInput.x = Input.mousePosition.x;
+        lookInput.y = Input.mousePosition.y;
+
+        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
+        mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
+
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+        rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
+
+        transform.Rotate(-mouseDistance.y * lookSpeed * Time.deltaTime, mouseDistance.x * lookSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
+
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxis("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxis("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxis("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
+
+        transform.position += (transform.forward * activeForwardSpeed * Time.deltaTime) + (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
     }
 
-    void turn()
-    {
-        // y axis movement
-        float yaw = turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
-        // x axis movement
-        float pitch = turnSpeed * Time.deltaTime * Input.GetAxis("Vertical");
-        // z axis movement
-        float roll = turnSpeed * Time.deltaTime * Input.GetAxis("Rotate");
-        playerShip.Rotate(pitch, yaw, roll);
-    }
 
-    void thrust()
-    {
-        playerShip.position += playerShip.forward * boostSpeed * Time.deltaTime * Input.GetAxis("Throttle");
-    }
 
 }
